67ab3a6f0460753dafd2314d4f54f6d5cffce9e6
jiwanwoo-97 <jwo9767@naver.com>
2024-05-05 21:34:36 +0900

    spring-swagger initial commit

---
+++ spring-swagger-test/src/main/java/com/springstudy/springswaggertest/SpringSwaggerTestApplication.java
@@ -0,0 +1,13 @@
+package com.springstudy.springswaggertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSwaggerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSwaggerTestApplication.class, args);
+    }
+
+}
