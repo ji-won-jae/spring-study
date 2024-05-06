b165ae265d557e0f47fd33974f416f1c286dc266
jiwanwoo-97 <jwo9767@naver.com>
2024-05-06 20:20:10 +0900

    spring-social-login initial commit

---
+++ spring-social-login/src/main/java/com/springstudy/springsociallogin/SpringSocialLoginApplication.java
@@ -0,0 +1,13 @@
+package com.springstudy.springsociallogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocialLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocialLoginApplication.class, args);
+	}
+
+}
---
+++ spring-social-login/src/test/java/com/springstudy/springsociallogin/SpringSocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springstudy.springsociallogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSocialLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
