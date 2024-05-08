241d40c6d95e3d6e02245dff637898c60705cf3e
jiwanwoo-97 <jwo9767@naver.com>
2024-05-08 22:51:41 +0900

    spring-stmp initial commit

---
+++ spring-stmp/src/main/java/com/springstudy/springstmp/SpringStmpApplication.java
@@ -0,0 +1,13 @@
+package com.springstudy.springstmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStmpApplication.class, args);
+	}
+
+}
